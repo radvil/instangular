226f67174e8beb8a3d5aaee65a07cbb37e20abc5
Radvil Ardian <radvil.developer@gmail.com>
2020-12-27 06:40:43 +0800

    💥 Create route for home

---
+++ angular-client/src/app/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeComponent } from './home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Home' },
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+
+    MatButtonModule,
+    MatIconModule,
+  ]
+})
+export class HomeModule { }
